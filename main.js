36f9215c0345c5f04fd155b7ef9ac021a5b45f33
Ted Zhu <tedz2usa@gmail.com>
2016-12-10 13:01:26 -0500

    Rectangular grid.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ window.onload = init;
 
 var canvas, ctx;
 var width, height;
+var gridSpace;
 
 function init() {
   log("Window Loaded!");
@@ -16,9 +17,23 @@ function init() {
   log(canvas);
   log(ctx);
 
-  ctx.strokeRect(20.5, 20.5, 100, 100);
-
   width = canvas.width;
   height = canvas.height;
+  gridSpace = 20;
 
+  drawGridDots();
+  drawRect(1, 1);
+
+}
+
+function drawGridDots() {
+  for (var gridX = 0; gridX < width/gridSpace; gridX++) {
+    for (var gridY = 0; gridY < height/gridSpace; gridY++) {
+      ctx.fillRect(gridX*gridSpace, gridY*gridSpace, 1, 1);
+    }
+  }
+}
+
+function drawRect(gridX, gridY) {
+  ctx.strokeRect(gridX * gridSpace+0.5, gridY * gridSpace+0.5, gridSpace, gridSpace);
 }
