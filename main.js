6bcd9b64346d1507a126866b468e2dd3c6f4bd1e
Ted Zhu <tedz2usa@gmail.com>
2016-12-10 12:23:16 -0500

    Get gameCanvas by id.

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ log("Hello!");
 
 window.onload = init;
 
+var canvas, ctx;
+
 function init() {
   log("Window Loaded!");
+
+  canvas = document.getElementById("gameCanvas");
+  ctx = canvas.getContext("2d");
+
+  log(canvas);
+  log(ctx);
+
 }
